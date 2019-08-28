a0034f4d20ebc8cdc7a76a0c044326b59db82721
goblin8621 <goblin8621@sk.com>
2019-08-28 16:48:34 +0900

    first commit

---
+++ src/main/java/com/sk/sample/siren/account/domain/model/MemberType.java
@@ -0,0 +1,6 @@
+package com.sk.sample.siren.account.domain.model;
+
+public enum MemberType {
+	SELLER,
+	BUYER
+}
---
+++ src/main/java/com/sk/sample/siren/account/domain/model/MembershipLevelType.java
@@ -0,0 +1,7 @@
+package com.sk.sample.siren.account.domain.model;
+
+public enum MembershipLevelType {
+	VIP,
+	GOLD,
+	SILVER
+}
